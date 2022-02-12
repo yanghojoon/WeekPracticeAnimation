4f9922c426ed349ed2dc6aa63c2d078bca060cc2
yanghojoon <yanghojoon8487@gmail.com>
2022-02-12 22:19:56 +0900

    feat: 새 맥북을 산 야곰 애니메이션 추가

--- WeekPracticeAnimation/ViewController.swift
+++ WeekPracticeAnimation/ViewController.swift
@@ -16,19 +16,13 @@ class ViewController: UIViewController {
     }
 
     @IBAction func touchUpMeetErrrorButton(_ sender: UIButton) {
-//        UIView.animate(withDuration: 5, delay: 0.0, options: [.repeat, .autoreverse]) {
-//            self.yagom.center = CGPoint(x: self.yagom.center.x - 50, y: self.yagom.center.y)
-//        } completion: { finished in
-//            print(self.yagom.center)
-//            print("에러를 만난 야곰이다~")
-//        }
-        
-        UIView.animateKeyframes(withDuration: 5, delay: 0.0, options: .repeat) {
+        UIView.animateKeyframes(withDuration: 0.1, delay: 0.0, options: .repeat) {
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5) {
-                self.yagom.center = CGPoint(x: self.yagom.center.x - 50, y: self.yagom.center.y)
+                self.yagom.center = CGPoint(x: self.yagom.center.x - 5, y: self.yagom.center.y)
+//                self.yagom.transform = CGAffineTransform(translationX: self.yagom.center.x - 50, y: self.yagom.center.y)
             }
             UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5) {
-                self.yagom.center = CGPoint(x: self.yagom.center.x + 100, y: self.yagom.center.y)
+                self.yagom.center = CGPoint(x: self.yagom.center.x + 10, y: self.yagom.center.y)
             }
         } completion: { finished in
             print("에러를 만난 야곰이다~")
@@ -36,8 +30,23 @@ class ViewController: UIViewController {
 
     }
     
+    @IBAction func touchUpBuyMac(_ sender: UIButton) {
+        UIView.animateKeyframes(withDuration: 1, delay: 0, options: .repeat) {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5) {
+                self.yagom.transform = CGAffineTransform(scaleX: 1.3, y: 0.8)
+            }
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5) {
+                self.yagom.transform = CGAffineTransform(scaleX: 0.8, y: 1.3)
+            }
+        } completion: { finished in
+            print("맥북을 사고 신난 야곰")
+        }
+
+    }
+    
     @IBAction func touchUpResetButton(_ sender: Any) {
         yagom.layer.removeAllAnimations()
+        self.yagom.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
     }
 }
 
