af17db922036ede157e9d72c204b0e9b52f63e9d
yanghojoon <yanghojoon8487@gmail.com>
2022-02-12 21:56:03 +0900

    refactor: 이미지가 왔다갔다 하도록 수정

--- WeekPracticeAnimation/ViewController.swift
+++ WeekPracticeAnimation/ViewController.swift
@@ -16,12 +16,24 @@ class ViewController: UIViewController {
     }
 
     @IBAction func touchUpMeetErrrorButton(_ sender: UIButton) {
-        UIView.animate(withDuration: 0.01, delay: 0.0, options: [.repeat, .autoreverse]) {
-            self.yagom.center = CGPoint(x: 155, y: 109)
+//        UIView.animate(withDuration: 5, delay: 0.0, options: [.repeat, .autoreverse]) {
+//            self.yagom.center = CGPoint(x: self.yagom.center.x - 50, y: self.yagom.center.y)
+//        } completion: { finished in
+//            print(self.yagom.center)
+//            print("에러를 만난 야곰이다~")
+//        }
+        
+        UIView.animateKeyframes(withDuration: 5, delay: 0.0, options: .repeat) {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5) {
+                self.yagom.center = CGPoint(x: self.yagom.center.x - 50, y: self.yagom.center.y)
+            }
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5) {
+                self.yagom.center = CGPoint(x: self.yagom.center.x + 100, y: self.yagom.center.y)
+            }
         } completion: { finished in
-            print(self.yagom.center)
             print("에러를 만난 야곰이다~")
         }
+
     }
     
     @IBAction func touchUpResetButton(_ sender: Any) {
