7079749e0cf7809ee658dfa5bfab6ab009397e3a
yanghojoon <yanghojoon8487@gmail.com>
2022-02-12 19:43:58 +0900

    feat: 에러를 만난 야곰 애니메이션 추가 및 reset 기능 추가

--- WeekPracticeAnimation/ViewController.swift
+++ WeekPracticeAnimation/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var yagom: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchUpMeetErrrorButton(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.01, delay: 0.0, options: [.repeat, .autoreverse]) {
+            self.yagom.center = CGPoint(x: 155, y: 109)
+        } completion: { finished in
+            print(self.yagom.center)
+            print("에러를 만난 야곰이다~")
+        }
+    }
+    
+    @IBAction func touchUpResetButton(_ sender: Any) {
+        yagom.layer.removeAllAnimations()
+    }
 }
 
